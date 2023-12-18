e56b66c92f9da4a21f9b47a869080e187b968042
Medachii <noe2laurent@gmail.com>
2023-12-17 21:22:10 -0500

    [+] Affichage

--- of_v0.12.0_vs_release/apps/myApps/emptyExample/RigidContactResolver.cpp
+++ of_v0.12.0_vs_release/apps/myApps/emptyExample/RigidContactResolver.cpp
@@ -1,31 +1,41 @@
 #include "RigidContactResolver.h"
 
-void RigidContactResolver::resolveContacts(vector<RigidContact*> contactArray, unsigned int numContacts, float duration)
+void RigidContactResolver::resolveContacts(vector<RigidContact*> contactArray, unsigned int numContacts, float duration, bool displayData)
 {
 	//Affichage des données
-	for (unsigned int i = 0; i < numContacts; i++) {
-		/*cout << "----------------------------------------------------------" << endl;
-		cout << "Contact " << i << " : " << endl;
-		cout << "Body 1 : " << contactArray[i]->body[0]->getPosition().getX() << " " << contactArray[i]->body[0]->getPosition().getY() << " " << contactArray[i]->body[0]->getPosition().getZ() << endl;
-		cout << "Body 2 : " << contactArray[i]->body[1]->getPosition().getX() << " " << contactArray[i]->body[1]->getPosition().getY() << " " << contactArray[i]->body[1]->getPosition().getZ() << endl;
-		cout << "Body 1 Velocity : " << contactArray[i]->body[0]->getVelocity().getX() << " " << contactArray[i]->body[0]->getVelocity().getY() << " " << contactArray[i]->body[0]->getVelocity().getZ() << endl;
-		cout << "Body 2 Velocity : " << contactArray[i]->body[1]->getVelocity().getX() << " " << contactArray[i]->body[1]->getVelocity().getY() << " " << contactArray[i]->body[1]->getVelocity().getZ() << endl;
-		cout << "Body 1 Angular Velocity : " << contactArray[i]->body[0]->getRotation().getX() << " " << contactArray[i]->body[0]->getRotation().getY() << " " << contactArray[i]->body[0]->getRotation().getZ() << endl;
-		cout << "Body 2 Angular Velocity : " << contactArray[i]->body[1]->getRotation().getX() << " " << contactArray[i]->body[1]->getRotation().getY() << " " << contactArray[i]->body[1]->getRotation().getZ() << endl;
-		float Mass1 = 1/contactArray[i]->body[0]->getInverseMass();
-		float Mass2 = 1/contactArray[i]->body[1]->getInverseMass();
+	if (displayData) {
+		for (unsigned int i = 0; i < numContacts; i++) {
 
-		cout << "Body 1 Mass : " << Mass1 << endl;
-		cout << "Body 2 Mass : " << Mass2 << endl;
+			cout << "----------------------------------------------------------" << endl;
+			cout << "Contact " << i << " : " << endl;
+			cout << "Body 1 : " << contactArray[i]->body[0]->getPosition().getX() << " " << contactArray[i]->body[0]->getPosition().getY() << " " << contactArray[i]->body[0]->getPosition().getZ() << endl;
+			cout << "Body 2 : " << contactArray[i]->body[1]->getPosition().getX() << " " << contactArray[i]->body[1]->getPosition().getY() << " " << contactArray[i]->body[1]->getPosition().getZ() << endl;
+			cout << "Body 1 Velocity : " << contactArray[i]->body[0]->getVelocity().getX() << " " << contactArray[i]->body[0]->getVelocity().getY() << " " << contactArray[i]->body[0]->getVelocity().getZ() << endl;
+			cout << "Body 2 Velocity : " << contactArray[i]->body[1]->getVelocity().getX() << " " << contactArray[i]->body[1]->getVelocity().getY() << " " << contactArray[i]->body[1]->getVelocity().getZ() << endl;
+			cout << "Body 1 Angular Velocity : " << contactArray[i]->body[0]->getRotation().getX() << " " << contactArray[i]->body[0]->getRotation().getY() << " " << contactArray[i]->body[0]->getRotation().getZ() << endl;
+			cout << "Body 2 Angular Velocity : " << contactArray[i]->body[1]->getRotation().getX() << " " << contactArray[i]->body[1]->getRotation().getY() << " " << contactArray[i]->body[1]->getRotation().getZ() << endl;
+			float Mass1 = 1 / contactArray[i]->body[0]->getInverseMass();
+			float Mass2 = 1 / contactArray[i]->body[1]->getInverseMass();
 
-		cout << "World Contact Point : " << contactArray[i]->worldContactPoint.getX() << " " << contactArray[i]->worldContactPoint.getY() << " " << contactArray[i]->worldContactPoint.getZ() << endl;
-		cout << "Contact Normal : " << contactArray[i]->contactNormal.getX() << " " << contactArray[i]->contactNormal.getY() << " " << contactArray[i]->contactNormal.getZ() << endl;
-		cout << "Penetration : " << contactArray[i]->penetration << endl;
-		cout << "Restitution : " << contactArray[i]->restitution << endl;
-		cout << "Friction : " << contactArray[i]->friction << endl;
-		cout << "----------------------------------------------------------" << endl;
-		cout << endl;*/
+			cout << "Body 1 Mass : " << Mass1 << endl;
+			cout << "Body 2 Mass : " << Mass2 << endl;
+
+			cout << "World Contact Point : " << contactArray[i]->worldContactPoint.getX() << " " << contactArray[i]->worldContactPoint.getY() << " " << contactArray[i]->worldContactPoint.getZ() << endl;
+			cout << "Contact Normal : " << contactArray[i]->contactNormal.getX() << " " << contactArray[i]->contactNormal.getY() << " " << contactArray[i]->contactNormal.getZ() << endl;
+			cout << "Penetration : " << contactArray[i]->penetration << endl;
+			cout << "Restitution : " << contactArray[i]->restitution << endl;
+			cout << "Friction : " << contactArray[i]->friction << endl;
+		
+			cout << endl;
+
+
+		}
+		if (numContacts != 0) {
+			cout << "################################################################" << endl;
+		}
+		
 	}
+
 
 
 
